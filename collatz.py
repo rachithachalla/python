988c7d7526c2b6a439951b604ff38c1d0b2803de
rachithachalla <51987147+rachithachalla@users.noreply.github.com>
2019-06-19 13:50:45 +0530

    Add files via upload

---
+++ collatz.py
@@ -0,0 +1,16 @@
+def getCollatzSequence(x):
+    # ADD YOUR CODE HERE
+   
+      # ADD YOUR CODE HERE
+   
+    res = [x]    
+    if x <= 0:
+       return []
+    while x > 1:
+         if x % 2 != 0:
+            x = (x * 3) + 1
+            res.append(x)
+         else:
+              x = x // 2
+              res.append(x)
+    return res
